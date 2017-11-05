183e9e4b21a3d0b40d734b01f3f89c2c5d5e3f95
Stegano <stegano@naver.com>
2017-11-05 22:33:15 +0900

    Update: Modify demo page.

--- demo/index.ts
+++ demo/index.ts
@@ -4,9 +4,32 @@
  * */
 import * as $ from "jquery";
 import { debounce } from "lodash";
-import Scatter from "../src/index";
+import  Scatter, { ScatterOptions } from "../src/index";
 import "./index.css";
-var scatter = new Scatter({});
+
+var scatter = new Scatter();
+
+// 윈도우 리사이징 이벤트.
 $(window).on("resize", debounce(() => {
     scatter.setResize(window.innerWidth, 800)
+}, 300));
+
+// 윈도우 리사이징 이벤트.
+$(window).on("keydown", debounce((e) => {
+    var options: ScatterOptions = scatter.getOptions();
+    switch (e.keyCode) {
+        case 38:
+            // ArrowUp
+            if (options.axisOptions.yAxis.max + 1e3 > 1e4) {
+                options.axisOptions.yAxis.max += 1e3;
+            }
+            break;
+        case 40:
+            // ArrowDown
+            if (options.axisOptions.yAxis.max - 1e3 > 0) {
+                options.axisOptions.yAxis.max -= 1e3;
+            }
+            break;
+    }
+    scatter.setOptions(options);
 }, 300));
