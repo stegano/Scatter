6aff6f1e0504fff5da220e3ed3966d1a373fba7a
Stegano <stegano@naver.com>
2017-11-06 21:08:07 +0900

    Update

--- demo/index.ts
+++ demo/index.ts
@@ -1,7 +1,3 @@
-/**
- * DEMO API
- * http://support.jennifersoft.com:27900/api/transaction/time?domain_id=7000&start_time=1508416235134&end_time=1508416295134
- * */
 import * as $ from "jquery";
 import { debounce } from "lodash";
 import  Scatter, { ScatterOptions } from "../src/index";
